bb7ade0bf48a6d19a4626da295e0746aeaa9213b
Michal <michal.szukalapl@interia.pl>
2023-03-26 21:38:46 +0200

    changes

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
             System.out.print("\n--------------------------------------------------------------------------\n");
             System.out.print("Wybierz numer zadania\n");
             System.out.print("1. Wyroznik delta i pierwiastki trojmianu kwadratowego\n");
-            System.out.print("2. Kalkulator\n");
-            System.out.print("5. Program wyświetlający liczby od 20-0, z wyłączeniem liczb {2,6,9,15,19\n");
+            System.out.print("2. Kalkulator i zadanie 3 i zadanie 4\n");
+            System.out.print("5. Program wyświetlający liczby od 20-0, z wyłączeniem liczb {2,6,9,15,19}\n");
             System.out.print("6. Petla nieskonczona\n");
             System.out.print("5. Podnoszenie do 3 potegi\n");
             System.out.print("7. Sortowanie\n");
@@ -101,6 +101,9 @@ class zadanie2 {
                 case 7:
                     funTryg();
                     break;
+                case 8:
+                    Zadanie3i4();
+                    break;
                 default:
                     System.out.println("Wprowadz wybor jeszcze raz");
             }
@@ -134,6 +137,7 @@ class zadanie2 {
         System.out.println("5.Potegowanie");
         System.out.println("6.Pierwiastkowanie");
         System.out.println("7.Funkcje trygonometryczne");
+        System.out.println("8.Zadanie 3 i 4");
         System.out.println("0.Zakoncz dzialanie programu");
     }
 
@@ -210,6 +214,137 @@ class zadanie2 {
         System.out.println("Wynik: " + wynik+"\n");
 
     }
+    public static void Zadanie3i4() {
+        double[] Liczby = new double[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Podaj " + (i + 1) + ". liczbę: ");
+            Liczby[i] = WprowadzanieDouble();
+        }
+
+        int wybor;
+
+        do{
+            System.out.print("\n--------------------------------------------------------------------------\n");
+            System.out.println("Wybierz operacje, ktore chcesz by program wykonal");
+            System.out.println("1.Wypiswanie od poczatku");
+            System.out.println("2.Wypiswyanie od tylu");
+            System.out.println("3.Wypisywanie nieparzystych");
+            System.out.println("4.Wypisywanie parzystych");
+            System.out.println("5.Wypisywanie sumy tablic");
+            System.out.println("6.Wypisywanie iloczynu tablic");
+            System.out.println("7.Wypisywanie sredniej");
+            System.out.println("8.Wypisywanie Min");
+            System.out.println("9.Wypisywanie Max");
+            System.out.println("0.Wroc do kalkulatora");
+            wybor = WprowadzanieInt();
+            switch (wybor){
+                case 1:
+                    OdPoczatku(Liczby);
+                    break;
+                case 2:
+                    OdTylu(Liczby);
+                    break;
+                case 3:
+                    Nieparzyste(Liczby);
+                    break;
+                case 4:
+                    Parzyste(Liczby);
+                    break;
+                case 5:
+                    SumeTablic(Liczby);
+                    break;
+                case 6:
+                    IloczynTablic(Liczby);
+                    break;
+                case 7:
+                    SredniaTablic(Liczby);
+                    break;
+                case 8:
+                    MinTablic(Liczby);
+                    break;
+                case 9:
+                    MaxTablic(Liczby);
+                    break;
+
+                default:
+                    System.out.println("Wprowadz wybor jeszcze raz");
+            }
+        }while (wybor != 0);
+
+    }
+    public static void OdPoczatku(double[] tablica) {
+        System.out.print("Tablica od poczatku: ");
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.print(tablica[i] + " ");
+        }
+    }
+
+    public static void OdTylu(double[] tablica) {
+        System.out.print("\nTablica od tylu: ");
+        for (int i = tablica.length - 1; i >= 0; i--) {
+            System.out.print(tablica[i] + " ");
+        }
+    }
+
+    public static void Nieparzyste(double[] tablica) {
+        System.out.print("\nNieparzyste: ");
+        for (int i = 1; i < tablica.length; i =i+ 2) {
+            System.out.print(tablica[i] + " ");
+        }
+    }
+
+    public static void Parzyste(double[] tablica) {
+        System.out.print("\nParzyste: ");
+        for (int i = 0; i < tablica.length; i = i + 2) {
+            System.out.print(tablica[i] + " ");
+        }
+    }
+    public static void SumeTablic(double[] tablica) {
+        double Suma = 0;
+        for (int i = 0; i < tablica.length; i++) {
+            Suma = Suma + tablica[i];
+        }
+        System.out.println("\nSuma: " + Suma);
+    }
+
+    public static void IloczynTablic(double[] tablica) {
+        double iloczyn = 1;
+        for (int i = 0; i < tablica.length; i++) {
+            iloczyn *= tablica[i];
+        }
+        System.out.println("Iloczyn: " + iloczyn);
+    }
+
+    public static void SredniaTablic(double[] tablica) {
+        double Suma = 0;
+        for (int i = 0; i < tablica.length; i++) {
+            Suma = Suma + tablica[i];
+        }
+        double srednia = Suma / tablica.length;
+        System.out.println("Średnia: " + srednia);
+    }
+
+    public static void MinTablic(double[] tablica) {
+        double Min = tablica[0];
+        for (int i = 1; i < tablica.length; i++) {
+            if (tablica[i] < Min) {
+                Min = tablica[i];
+            }
+        }
+        System.out.println("Min: " + Min);
+    }
+
+    public static void MaxTablic(double[] tablica) {
+        double Max = tablica[0];
+        for (int i = 1; i < tablica.length; i++) {
+            if (tablica[i] > Max) {
+                Max = tablica[i];
+            }
+        }
+        System.out.println("Max: " + Max);
+    }
+
+
     public static void Close() {
         System.out.println("\t\t\tCzy na pewno chcesz wyjsc t || T");
         String znak = WprowadzanieString();
